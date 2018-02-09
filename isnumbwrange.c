eb65b5055e6f2c42124dea671f5a014d1fa7046c
sowbarnigavsb <31504504+sowbarnigavsb@users.noreply.github.com>
2018-02-09 14:40:44 +0530

    Create isnumbwrange.c

---
+++ isnumbwrange.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+	int n,l,r,i;
+	scanf("%d%d%d",&n,&l,&r);
+	for(i=l;i<=r;i++)
+           {
+           if(i==n)
+           printf("yes");
+           }
+	return 0;
+}
